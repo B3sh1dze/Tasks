d6ffb5f3ce6b4d413a3b5e6bf759d8425705bcde
B3sh1dze <beshidzeguro@gmail.com>
2023-01-11 12:17:22 +0400

    ganaxlebuli RPS

--- RockPaperScissorsInCSharp/Program.cs
+++ RockPaperScissorsInCSharp/Program.cs
@@ -1,101 +1,104 @@
 ﻿int computerWins = 0;
 int playersWins = 0;
-while(true)
+string[] moves = { "rock", "paper", "scissors" };
+while (true)
 {
     Console.Write("Please enter the character you want from('r','s','p'): ");
     char playersInput = Convert.ToChar(Console.ReadLine());
-    if (playersInput == 'r')
+    if (playersInput != 'r' && playersInput != 's' && playersInput != 'p')
     {
-        Console.WriteLine($"Player's input is rock");
+        Console.WriteLine("wrong input! please enter the correct character! ");
+        continue;
     }
-    else if (playersInput == 's')
-    {
-        Console.WriteLine($"Player's input is scissors");
-    }
-    else if (playersInput == 'p')
-    {
-        Console.WriteLine($"Player's input is paper");
-    }
-    else
-    {
-        Console.WriteLine("Invalid input! please enter the correct character.");
-    }
+    string playerMove = moves[playersInput - 'r'];
+    Console.WriteLine($"Player's move is: {playerMove}");
     // generate random numbers to show computer's move
     var random = new Random();
-    int randomNumber = random.Next(1, 3);
-    string computersMove;
-    if (randomNumber == 1)
+    int randomNumber = random.Next(0, 3);
+    string computersMove = moves[randomNumber];
+    Console.WriteLine($"computer's move: {computersMove}");
+    if (playersInput == 'r' && computersMove == "scissors" || playersInput == 's' && computersMove == "paper" || playersInput == 'p' && computersMove == "rock") 
     {
-        computersMove = "rock";
-        Console.WriteLine($"Computer's input is: {computersMove}");
+        Console.WriteLine("player wins!");
+        playersWins++;
     }
-    else if (randomNumber == 2)
+    else if(playersInput == 'r' && computersMove == "rock" || playersInput == 's' && computersMove == "scissors" || playersInput == 'p' && computersMove == "paper")
     {
-        computersMove = "paper";
-        Console.WriteLine($"Computer's input is: {computersMove}");
+        Console.WriteLine("It's a tie!");
     }
     else
-    {
-        computersMove = "scissors";
-        Console.WriteLine($"Computer's input is: {computersMove}");
-    }
-
-    // check the winner
-    if (playersInput == 'r' && computersMove == "rock")
-    {
-        Console.WriteLine("It's a tie!");
-    }
-    else if (playersInput == 'r' && computersMove == "paper")
-    {
-        Console.WriteLine("Computer wins!");
-        computerWins++;
-    }
-    else if (playersInput == 'r' && computersMove == "scissors")
-    {
-        Console.WriteLine("player wins!");
-        playersWins++;
-    }
-    else if (playersInput == 's' && computersMove == "scissors")
-    {
-        Console.WriteLine("It's a tie!");
-    }
-    else if (playersInput == 's' && computersMove == "paper")
-    {
-        Console.WriteLine("player wins!");
-        playersWins++;
-    }
-    else if (playersInput == 's' && computersMove == "rock")
-    {
-        Console.WriteLine("computer wins!");
-        computerWins++;
-    }
-    else if (playersInput == 'p' && computersMove == "paper")
-    {
-        Console.WriteLine("It's a tie!");
-    }
-    else if (playersInput == 'p' && computersMove == "rock")
-    {
-        Console.WriteLine("player wins!");
-        playersWins++;
-    }
-    else if (playersInput == 'p' && computersMove == "scissors")
     {
         Console.WriteLine("computer wins!");
         computerWins++;
     }
     Console.WriteLine($"computer's score: {computerWins}");
-    Console.WriteLine($"player's score: {playersWins}");
+    Console.WriteLine($"player's's score: {playersWins}");
     if (computerWins == 3)
     {
-        Console.WriteLine("computer won this game.");
-    }
-    else if (playersWins == 3)
-    {
-        Console.WriteLine("player won this game.");
-    }
-    if (computerWins == 3 || playersWins == 3)
-    {
+        Console.WriteLine("computer wins the game!!!");
         break;
     }
-    
+    else if(playersWins == 3)
+    {
+        Console.WriteLine("player wins the game!!!");
+        break;
+    }
 }
+    // check the winner
+//    if (playersInput == 'r' && computersMove == "rock")
+//    {
+//        Console.WriteLine("It's a tie!");
+//    }
+//    else if (playersInput == 'r' && computersMove == "paper")
+//    {
+//        Console.WriteLine("Computer wins!");
+//        computerWins++;
+//    }
+//    else if (playersInput == 'r' && computersMove == "scissors")
+//    {
+//        Console.WriteLine("player wins!");
+//        playersWins++;
+//    }
+//    else if (playersInput == 's' && computersMove == "scissors")
+//    {
+//        Console.WriteLine("It's a tie!");
+//    }
+//    else if (playersInput == 's' && computersMove == "paper")
+//    {
+//        Console.WriteLine("player wins!");
+//        playersWins++;
+//    }
+//    else if (playersInput == 's' && computersMove == "rock")
+//    {
+//        Console.WriteLine("computer wins!");
+//        computerWins++;
+//    }
+//    else if (playersInput == 'p' && computersMove == "paper")
+//    {
+//        Console.WriteLine("It's a tie!");
+//    }
+//    else if (playersInput == 'p' && computersMove == "rock")
+//    {
+//        Console.WriteLine("player wins!");
+//        playersWins++;
+//    }
+//    else if (playersInput == 'p' && computersMove == "scissors")
+//    {
+//        Console.WriteLine("computer wins!");
+//        computerWins++;
+//    }
+//    Console.WriteLine($"computer's score: {computerWins}");
+//    Console.WriteLine($"player's score: {playersWins}");
+//    if (computerWins == 3)
+//    {
+//        Console.WriteLine("computer won this game.");
+//    }
+//    else if (playersWins == 3)
+//    {
+//        Console.WriteLine("player won this game.");
+//    }
+//    if (computerWins == 3 || playersWins == 3)
+//    {
+//        break;
+//    }
+//}
