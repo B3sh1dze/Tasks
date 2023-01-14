0f85dd7f140817b0a7522a7c3efcc9f3a710f684
B3sh1dze <beshidzeguro@gmail.com>
2023-01-14 17:26:07 +0400

    უბრალოდ ტასკები

--- task 3.1/Program.cs
+++ task 3.1/Program.cs
@@ -134,3 +134,78 @@
 //    Console.Write(c + " ");
 //}
 
+
+//// w3resources string exercises task 1
+//Console.Write("Please input string you want: ");
+//string str = Console.ReadLine();
+//Console.WriteLine($"Your entered string is: {str}");
+
+//// task 2
+//Console.Write("Please input string you want: ");
+//string str = Console.ReadLine();
+//Console.WriteLine($"Your entered string's length is: {str.Length}");
+
+//// task 3
+//Console.Write("Please input string you want: ");
+//string str = Console.ReadLine();
+//char[] characters = str.ToCharArray();
+//Console.Write($"The characters of entered string are: ");
+//foreach(char c in characters )
+//{
+//    Console.Write($"{c} ");
+//}
+
+//// task 4   printing reversed array
+//Console.Write("Please input string you want: ");
+//string str = Console.ReadLine();
+//char[] characters = str.ToCharArray();
+//Array.Reverse(characters);
+//Console.Write($"The entered strings reverse characters are: ");
+//for(int i = 0; i < characters.Length; i++)
+//{
+//    Console.Write($"{characters[i]} ");
+//}
+
+//// task 5    counting the words in string
+//Console.Write("Please input string you want: ");
+//string str = Console.ReadLine();
+//string[] words = str.Split(' ');
+//int wordCount = words.Length;
+//Console.WriteLine($"The total number of words in the string is: {wordCount}");
+
+//// task 6
+//Console.Write("Please input first string: ");
+//string str = Console.ReadLine();
+//Console.Write("Please input second string: ");
+//string str2 = Console.ReadLine();
+//int a = str.Length;
+//int b = str2.Length;
+//if(a == b && str == str2)
+//{
+//    Console.WriteLine("The length of both strings are equal and \r\nalso, both strings are equal.");
+//}
+//else if (a == b)
+//{
+//    Console.WriteLine("The length of these strings is equal");
+//}
+//else
+//{
+//    Console.WriteLine("The length of these strings aren't equal.");
+//}
+
+
+//// string concatenation
+//string firstName = "Guro";
+//string lastName = " Beshidze";
+//string fullName = string.Concat(firstName, lastName);
+//Console.WriteLine(fullName);
+
+// substring function
+string name = "Guro beshidze";
+int index = name.IndexOf('b');
+string lastName = name.Substring(index, 3);
+string lastName1 = name.Substring(3, index);
+string lastName2 = name.Substring(index);
+Console.WriteLine(lastName);
+Console.WriteLine(lastName1);
+Console.WriteLine(lastName2);
