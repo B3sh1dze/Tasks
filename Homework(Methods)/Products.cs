67d045cb6e1cd0d9c6f63690a4c6d0fe49512015
B3sh1dze <beshidzeguro@gmail.com>
2023-02-05 16:00:21 +0400

    WarehouseProducts

---
+++ Homework(Methods)/Products.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_Methods_
+{
+    public class Products : Warehouse
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public double Price { get; set; } = new Random().NextDouble() * 22 + 3;
+        public int Stock { get; set; } = new Random().Next(5, 30);
+        public List<string> PossibleCategories = new List<string> { "Dry Food", "Diary", "Sports Inventory", "Beverages" };
+        public Products()
+        {
+
+        }
+        public Products(string name, string category)
+        {
+            Name = name;
+            Category = category;
+        }
+        public Products(double price, int stock)
+        {
+            Price = price;
+            Stock = stock;
+        }
+        public Products(string name, string category, double price, int stock)
+        {
+            Name = name;
+            Category = category;
+            Price = price;
+            Stock = stock;
+        }
+        public void GenerateProducts()
+        {
+            var productNames = new List<string>()
+            {
+                "Milk",
+                "Crisps",
+                "Espander",
+                "Sparkling Water",
+                "Yogurt",
+                "Beer"
+            };
+            var productCategory = new List<string>()
+            {
+                "Diary",
+                "Dry Food",
+                "Sprost Inventory",
+                "Beverages",
+                "Diary",
+                "Beverages"
+            };
+            var products = new List<Products>();
+            for (int i = 0; i < productNames.Count; i++)
+            {
+                var product = new Products()
+                {
+                    Name = productNames[i],
+                    Category = productCategory[i],
+                };
+                ProductsList.Add(product);
+            }
+        }
+        public override string ToString()
+        {
+            double roundedNumber = Math.Round(Price, 2);
+            return
+                $"name: {Name}\n" +
+                $"Category: {Category}\n" +
+                $"price: {roundedNumber}\n" +
+                $"stock: {Stock}";
+        }
+    }
+}
---
+++ Homework(Methods)/Warehouse.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Homework_Methods_
+{
+    public class Warehouse
+    {
+        public List<Products> ProductsList { get; set; } = new List<Products>();
+        public bool IsProductNameValid(string name)
+        {
+            if (name == null)
+            {
+                return false;
+            }
+            else if (Regex.IsMatch(name, @"^\d"))
+            {
+                return false;
+            }
+            else if (name.StartsWith(" ") || name.EndsWith(" "))
+            {
+                return false;
+            }
+            else if (name.Length > 50)
+            {
+                return false;
+            }
+            foreach (var item in ProductsList)
+            {
+                if (name == item.Name)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public void RegisterProducts()
+        {
+            Console.Write("Please enter products name: ");
+            var name = Console.ReadLine();
+            bool isProductNameValid = IsProductNameValid(name);
+            if (isProductNameValid)
+            {
+                var category = ChooseCategory();
+                Console.WriteLine();
+                Console.Write("Enter price: ");
+                double price = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter stock amount: ");
+                int stock = Convert.ToInt32(Console.ReadLine());
+                var isPriceAndStockCorrect = IsProductPriceAndStockValid(price, stock);
+                if (isPriceAndStockCorrect)
+                {
+                    var product = new Products(name, category, price, stock);
+                    ProductsList.Add(product);
+                    Console.WriteLine();
+                    Console.WriteLine("You successfuly added product: ");
+                    Console.WriteLine(product);
+                }
+                else
+                {
+                    Console.WriteLine("Wrong format.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong format.");
+            }
+        }
+        public bool IsProductPriceAndStockValid(double price, int stock)
+        {
+            if(price <= 0 || stock < 0)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void UpdateProductPriceAndAmount()
+        {
+            Console.Clear();
+            ShowAllProducts();
+            Console.Write("Choose product you want to Change: ");
+            var userChoice = Convert.ToInt32(Console.ReadLine());
+            var usersChoice = ProductsList[userChoice - 1];
+            Console.WriteLine("You chose: ");
+            Console.WriteLine(usersChoice);
+            Console.Write("Enter new Price: ");
+            var newPrice = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter new stock amount: ");
+            var newStock = Convert.ToInt32(Console.ReadLine());
+            usersChoice.Price = newPrice;
+            usersChoice.Stock = newStock;
+            Console.Clear();
+            Console.WriteLine("Changed Product: ");
+            Console.WriteLine(usersChoice);
+        }
+        public void RemoveRegisteredProduct()
+        {
+            ShowAllProducts();
+            Console.Write("Choose product you want to remove: ");
+            var userChoice = Convert.ToInt32(Console.ReadLine());
+            var usersChoice = ProductsList[userChoice - 1];
+            ProductsList.Remove(usersChoice);
+            Console.Clear();
+            Console.WriteLine("You removed: ");
+            Console.WriteLine(usersChoice);
+            Console.WriteLine();
+        }
+        public void ShowAllProducts()
+        {
+            Console.Clear();
+            int i = 1;
+            foreach (var item in ProductsList)
+            {
+                Console.Write(i + ". ");
+                Console.WriteLine(item.ToString());
+                i++;
+            }
+        }
+        public string ChooseCategory()
+        {
+            Console.WriteLine("Please choose products category: ");
+            var product = new Products();
+            int i = 1;
+            foreach (var item in product.PossibleCategories)
+            {
+                Console.Write(i + ". ");
+                Console.WriteLine(item);
+                i++;
+            }
+            var categoryChoice = Console.ReadKey().Key;
+            if(categoryChoice == ConsoleKey.D1)
+            {
+                return "Dry Food";
+            }
+            else if(categoryChoice == ConsoleKey.D2)
+            {
+                return "Diary";
+            }
+            else if(categoryChoice == ConsoleKey.D3)
+            {
+                return "Sports Inventory";
+            }
+            else if(categoryChoice == ConsoleKey.D4)
+            {
+                return "Beverages";
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void DisplayWarehouseOptionsMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("1 - Register Products");
+            Console.WriteLine("2 - Update products price and amount");
+            Console.WriteLine("3 - Remove registered product");
+            Console.WriteLine("4 - Show all products");
+            Console.WriteLine("5 - Exit");
+        }
+        public void ChoiceInMenu()
+        {
+            while (true)
+            {
+                DisplayWarehouseOptionsMenu();
+                var choice = Console.ReadKey().Key;
+                if (choice == ConsoleKey.D1)
+                {
+                    Console.WriteLine();
+                    RegisterProducts();
+                }
+                else if (choice == ConsoleKey.D2)
+                {
+                    Console.WriteLine();
+                    UpdateProductPriceAndAmount();
+                }
+                else if (choice == ConsoleKey.D3)
+                {
+                    Console.WriteLine();
+                    RemoveRegisteredProduct();
+                }
+                else if (choice == ConsoleKey.D4)
+                {
+                    Console.WriteLine();
+                    ShowAllProducts();
+                }
+                else if (choice == ConsoleKey.D5)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Thanks for visiting.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Wrong input. try again!");
+                }
+            }
+        }
+    }
+}
